98d48e25ea3b5456b40ce0d627bd27da3086a72e
yarkin <nikita.kondratjuks@student.uib.no>
2022-04-24 01:51:36 +0200

    The plot thicens

--- src/Game/Storyline.java
+++ src/Game/Storyline.java
@@ -1,6 +1,7 @@
 package Game;
 
 import Equipment.Weapons.WeaponDagger;
+import Equipment.Weapons.WeaponLongsword;
 import GUI.UI.VisibilityManager;
 import GUI.UIscreen;
 import Music.Sound;
@@ -48,6 +49,15 @@ public class Storyline {
             case "backToSleep" -> backToSleep();
             case "talkToBartender2" -> talkToBartender2();
             case "talkToBartender3" -> talkToBartender3();
+            case "north1Forest" -> north1Forest();
+            case "east1Round" -> east1Road();
+            case "west1Smith" -> west1Smith();
+            case "south1Swamp" -> south1Swamp();
+            case "north2Sword" -> north2Sword();
+            case "bushEncounter" -> bushEncounter();
+            case "gameover2" -> gameover2();
+            case "swordEquipped" -> swordEquipped();
+            case "north2NoSword" -> north2NoSword();
         }
     }
 
@@ -94,7 +104,7 @@ public class Storyline {
 
     }
     public void talkToBartender2(){
-        uIscreen.mainTextArea.setText("Human Bartender: *Laughs* man you dont remember anything. \nDisregards you question.");
+        uIscreen.mainTextArea.setText("Human Bartender: *Laughs* man you dont remember anything. \nDisregards your question.");
         // Dont the characters longer the 21 characters or they go outside the box.
         uIscreen.choice1.setText("Ask about Rumours");
         uIscreen.choice2.setText("Last night?");
@@ -108,16 +118,162 @@ public class Storyline {
 
     }
     public void talkToBartender3(){
+        uIscreen.mainTextArea.setText("Human Bartender: Rumours you ask? Aye there are goblins in to the east of the east of the village, and a dangourouse Troll to the south");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("Thank you! I will go");
+        uIscreen.choice2.setText("Last night?");
+        uIscreen.choice3.setText("Who is that peasant?");
+        uIscreen.choice4.setText("Go outside");
+
+        game.nextPosition1 = "goOutside";
+        game.nextPosition2 = "talkToBartender2";
+        game.nextPosition3 = "talkToBartender2";
+        game.nextPosition4 = "goOutside";
 
     }
     public void wakePeasant(){
+        uIscreen.mainTextArea.setText("");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("");
 
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "";
     }
     public void goOutside(){
+        uIscreen.mainTextArea.setText("You feel the fresh air hit your face. Adventure awaits \nWhere will you travel");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("north");
+        uIscreen.choice2.setText("east");
+        uIscreen.choice3.setText("west");
+        uIscreen.choice4.setText("south");
 
+        game.nextPosition1 = "north1Forest";
+        game.nextPosition2 = "east1Road";
+        game.nextPosition3 = "west1Smith";
+        game.nextPosition4 = "south1Swamp";
     }
     public void backToSleep(){
+        uIscreen.mainTextArea.setText("As you fall a sleep for the final time. It seems like you dont wake up");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("");
 
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "";
+    }
+
+    public void north1Forest(){
+        uIscreen.mainTextArea.setText("As you walk north you reach a forest. The road is long.\n Your hear sound from a bush near you \nWhat will you do?");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("Follow the road");
+        uIscreen.choice2.setText("Search the bush");
+        uIscreen.choice3.setText("Go off the road");
+        uIscreen.choice4.setText("Go back");
+
+        game.nextPosition1 = "north2Sword";
+        game.nextPosition2 = "bushEncounter";
+        game.nextPosition3 = "gameover2";
+        game.nextPosition4 = "goOutside";
+    }
+    public void north2Sword(){
+        uIscreen.mainTextArea.setText("As you walk, following the road. You spot a sword stuck in a rock. \n What do you do?");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("Pull out the sword");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("Go back");
+
+        game.nextPosition1 = "swordEquipped";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "north1Forest";
+    }
+    public void swordEquipped(){
+        uIscreen.mainTextArea.setText("As you grab the hilt of the sword. With all your might you pull it out. \n Your aquire a longsword");
+
+        player.equippedWeapon = new WeaponLongsword();
+        uIscreen.weaponNameLabel.setText(player.equippedWeapon.name);
+
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("Go back");
+
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "north2NoSword";
+    }
+
+    public void north2NoSword(){
+        uIscreen.mainTextArea.setText("As you stand back. You look at the rock you pulled the sword from.\nWhat do you do?");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("Go back");
+
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "north1Forest";
+
+    }
+    public void bushEncounter(){
+
+    }
+    public void gameover2(){
+
+    }
+
+
+    public void east1Road(){
+        uIscreen.mainTextArea.setText("");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("");
+
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "";
+    }
+    public void west1Smith(){
+        uIscreen.mainTextArea.setText("");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("");
+
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "";
+    }
+    public void south1Swamp(){
+        uIscreen.mainTextArea.setText("");
+        // Dont the characters longer the 21 characters or they go outside the box.
+        uIscreen.choice1.setText("");
+        uIscreen.choice2.setText("");
+        uIscreen.choice3.setText("");
+        uIscreen.choice4.setText("");
+
+        game.nextPosition1 = "";
+        game.nextPosition2 = "";
+        game.nextPosition3 = "";
+        game.nextPosition4 = "";
     }
 
 
